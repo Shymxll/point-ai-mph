b2022c43e10a4b32782422ea90586c5ad7f1073a
Shamkhal Huseynzade <shamkhalhuseynzade26@gmail.com>
2025-01-09 20:34:03 +0300

    en to tr in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="tr">
       <head>
         <link rel="icon" href="/src/app/logo.ico" />
 
